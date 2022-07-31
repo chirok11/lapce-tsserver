218dfbb036c9096fd5e7e3e4e0d6e9198f499c9c
XoXo <chirok11@protonmail.ch>
2022-07-31 16:04:23 +0400

    0.1.0

--- src/main.rs
+++ src/main.rs
@@ -31,63 +31,7 @@ register_plugin!(State);
 impl LapcePlugin for State {
     fn initialize(&mut self, info: serde_json::Value) {
         let info = serde_json::from_value::<PluginInfo>(info).unwrap();
-        let arch = match info.arch.as_str() {
-            "x86_64" => "x86_64",
-            "aarch64" => "aarch64",
-            _ => return,
-        };
-        let os = match info.os.as_str() {
-            "linux" => "unknown-linux-gnu",
-            "macos" => "apple-darwin",
-            "windows" => "pc-windows-msvc",
-            _ => return,
-        };
-        let file_name = format!("rust-analyzer-{}-{}", arch, os);
-        let lock_file = PathBuf::from("donwload.lock");
-        send_notification(
-            "lock_file",
-            &json!({
-                "path": &lock_file,
-            }),
-        );
-        if !PathBuf::from(&file_name).exists() {
-            let url = format!(
-                "https://github.com/rust-analyzer/rust-analyzer/releases/download/2022-07-18/{}.gz",
-                file_name
-            );
-            let gz_file = PathBuf::from(file_name.clone() + ".gz");
-
-            if gz_file.exists() {
-                std::fs::remove_file(&gz_file);
-            }
-
-            {
-                send_notification(
-                    "download_file",
-                    &json!({
-                        "url": url,
-                        "path": gz_file,
-                    }),
-                );
-                if !gz_file.exists() {
-                    std::fs::remove_file(&lock_file);
-                    return;
-                }
-                eprintln!("start to unzip");
-                let mut gz = GzDecoder::new(File::open(&gz_file).unwrap());
-                let mut lsp_file = File::create(&file_name).unwrap();
-                std::io::copy(&mut gz, &mut lsp_file).unwrap();
-                send_notification(
-                    "make_file_executable",
-                    &json!({
-                        "path": file_name,
-                    }),
-                );
-            }
-            std::fs::remove_file(gz_file);
-        }
-        std::fs::remove_file(&lock_file);
-
-        start_lsp(&file_name, "rust", info.configuration.options);
+        let file_name = "/usr/bin/typescript-language-server";
+        start_lsp(&file_name, "js", info.configuration.options);
     }
 }
